82f517a21b88ecedfdcd16182865ae3e1af221bb
fellyph <fellyph.cintra@gmail.com>
2019-03-10 12:19:11 +0000

    adding new file-loader urle

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(svg|gif|png|jpe?g)$/,
+        test: /\.(woff(2)?|ttf|eot|svg|gif|png|jpe?g)$/,
         loader: 'url-loader',
         options: {
           limit: 100,
