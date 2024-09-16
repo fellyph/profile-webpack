e064abf3090fb4d409864bd93cbafe19d083c057
Fellyph Cintra <fellyph.cintra@gmail.com>
2024-09-16 17:17:55 +0100

    converting containerBlock to typescript

--- components/ContainerBlock.tsx
+++ components/ContainerBlock.tsx
@@ -4,24 +4,46 @@ import { useRouter } from "next/router";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-export default function ContainerBlock({ children, ...customMeta }) {
+interface ContainerBlockProps {
+  children: React.ReactNode;
+  title: string;
+  description: string;
+}
+
+export default function ContainerBlock({
+  children,
+  ...customMeta
+}: ContainerBlockProps) {
   const router = useRouter();
 
   const meta = {
-    title: "Fellyph Cintra - Developer and Content Creator",
-    description: 'Brazilian Front-end Engineer and Content Creator based in Lagos, Portugal',
     image: "/avatar.webp",
     type: "website",
     ...customMeta,
   };
+
   return (
     <div>
       <Head>
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#000000" />
@@ -30,10 +52,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
           property="og:url"
           content={`https://fellyph.com.br${router.asPath}`}
         />
-        <link
-          rel="canonical"
-          href={`https://fellyph.com.br${router.asPath}`}
-        />
+        <link rel="canonical" href={`https://fellyph.com.br${router.asPath}`} />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Fellyph Cintra" />
         <meta property="og:description" content={meta.description} />
@@ -44,9 +63,6 @@ export default function ContainerBlock({ children, ...customMeta }) {
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
-        {meta.date && (
-          <meta property="article:published_time" content={meta.date} />
-        )}
       </Head>
       <main className="dark:bg-gray-800 w-full">
         <Navbar />
