376cdcf28bcd43558a189315a0a9de5bc7c6ef98
Fellyph Cintra <fellyph.cintra@gmail.com>
2024-09-18 11:52:52 +0100

    creating a video card component

---
+++ components/VideoCard.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+const VideoCard = ({ video, index }: { video: Video; index: number }) => (
+  <a
+    key={video.id}
+    href={`https://www.youtube.com/watch?v=${video.id}`}
+    className={`w-full block ${
+      index === 0 ? "col-span-3" : "col-span-3 sm:col-span-1"
+    } shadow-2xl`}
+  >
+    <div className="relative overflow-hidden">
+      <Image
+        src={video.thumbnail}
+        alt={video.title}
+        width={index === 0 ? 1200 : 500}
+        height={index === 0 ? 900 : 281}
+        className="transform hover:scale-125 transition duration-2000 ease-out"
+      />
+      <h3
+        className={`absolute top-10 left-10 text-gray-50 font-bold  ${
+          index === 0 ? "text-xl" : "text-xs"
+        } bg-red-500 rounded-md px-2`}
+      >
+        {video.title}
+      </h3>
+      <h4
+        className={`absolute bottom-10 left-10 text-gray-50 font-bold  ${
+          index === 0 ? "text-xl" : "text-xs"
+        }`}
+      >
+        {String(index + 1).padStart(2, "0")}
+      </h4>
+    </div>
+  </a>
+);
+
+export default VideoCard;
