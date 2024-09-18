c039959bc701b4eda32aca9a3c052cbe26c17e86
Fellyph Cintra <fellyph.cintra@gmail.com>
2024-09-18 11:54:14 +0100

    fixing youtube api request

--- lib/getLatestVideos.ts
+++ lib/getLatestVideos.ts
@@ -2,14 +2,19 @@ import axios from "axios";
 
 // You'll need to replace this with your actual YouTube API key and channel ID
 const API_KEY = "AIzaSyDM0urCp-srXRqOFvSMXY4RI4cUgLdJeZA";
-const CHANNEL_ID = "UCu6DycMkhHF64HtB9nUaQiw";
+const CHANNEL_ID = "UCPaufJocHYVHj44iwXG95PA";
 
 const getLatestVideos = async (): Promise<Video[]> => {
   try {
-    const res = await axios.get<Video[]>(
+    const res = await axios.get<YouTubeApiResponse>(
       `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=4`
     );
-    return res.data;
+    return res.data.items.map((item) => ({
+      id: item.id.videoId,
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.medium.url,
+      publishedAt: item.snippet.publishedAt,
+    }));
   } catch (err) {
     console.error(err);
     return [];
